f3d323a823da56e9061cb17dbf7bd57c8eacf23d
Ronald Otieno <ronnlats@gmail.com>
2023-08-11 17:35:19 +0000

    favicon

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.6d677c81.js","_app/immutable/chunks/scheduler.d3beae7d.js","_app/immutable/chunks/index.af8cfb0a.js","_app/immutable/chunks/stores.d3fa26b3.js","_app/immutable/chunks/singletons.8c830515.js","_app/immutable/chunks/index.41200950.js"];
+export const imports = ["_app/immutable/nodes/1.cdd4a039.js","_app/immutable/chunks/scheduler.d3beae7d.js","_app/immutable/chunks/index.af8cfb0a.js","_app/immutable/chunks/stores.7aba5978.js","_app/immutable/chunks/singletons.09663172.js","_app/immutable/chunks/index.41200950.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/4.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/4.js
@@ -5,6 +5,6 @@ let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/category/_categoryName_/_page.svelte.js')).default;
 export { universal };
 export const universal_id = "src/routes/category/[categoryName]/+page.js";
-export const imports = ["_app/immutable/nodes/4.e0f3575f.js","_app/immutable/chunks/supabaseClient.90111ed0.js","_app/immutable/chunks/preload-helper.cf010ec4.js","_app/immutable/chunks/scheduler.d3beae7d.js","_app/immutable/chunks/index.af8cfb0a.js","_app/immutable/chunks/products.348e33fd.js","_app/immutable/chunks/index.41200950.js","_app/immutable/chunks/stores.d3fa26b3.js","_app/immutable/chunks/singletons.8c830515.js"];
+export const imports = ["_app/immutable/nodes/4.df115207.js","_app/immutable/chunks/supabaseClient.90111ed0.js","_app/immutable/chunks/preload-helper.cf010ec4.js","_app/immutable/chunks/scheduler.d3beae7d.js","_app/immutable/chunks/index.af8cfb0a.js","_app/immutable/chunks/products.348e33fd.js","_app/immutable/chunks/index.41200950.js","_app/immutable/chunks/stores.7aba5978.js","_app/immutable/chunks/singletons.09663172.js"];
 export const stylesheets = [];
 export const fonts = [];
