49157857c32da50a44203ce4147297549e32e1f4
Ronald Otieno <ronnlats@gmail.com>
2023-08-28 23:10:27 +0300

    hide footer items

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -15,7 +15,7 @@
 				>
 			</a>
 			<ul
-				class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400"
+				class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 hidden"
 			>
 				<li>
 					<a href="#" class="mr-4 hover:underline md:mr-6">About</a>
