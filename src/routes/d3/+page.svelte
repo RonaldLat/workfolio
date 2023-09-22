cb971ddcf8aa946fc7f877d060b6df0ca4b26c53
Ronald Otieno <ronnlats@gmail.com>
2023-09-22 17:15:52 +0000

    d3

--- src/routes/d3/+page.svelte
+++ src/routes/d3/+page.svelte
@@ -1,42 +1,16 @@
-<!-- BarChart.svelte -->
 <script>
-  import { onMount } from 'svelte';
-  import * as d3 from "d3";
+  import LinePlot from '$lib/components/LinePlot.svelte';
+import * as d3 from 'd3';
 
+	  let data = d3.ticks(-2, 2, 200).map(Math.sin);
 
-  // Data for the bar chart (you can replace this with your own data)
-  let data = [10, 20, 30, 40, 50];
-
-  // Select the container element
-  let container;
-
-  onMount(() => {
-    // Set up the SVG canvas
-    const svg = d3.select(container)
-      .append('svg')
-      .attr('width', 400)
-      .attr('height', 200);
-
-    // Create and render the bars
-    svg.selectAll('rect')
-      .data(data)
-      .enter()
-      .append('rect')
-      .attr('x', (d, i) => i * 60)
-      .attr('y', (d) => 200 - d)
-      .attr('width', 40)
-      .attr('height', (d) => d)
-      .attr('fill', 'teal');
-  });
+	  function onMousemove(event) {
+			const [x, y] = d3.pointer(event);
+	    data = data.slice(-200).concat(Math.atan2(x, y));
+	  }
 </script>
 
-<h2>D3</h2>
-<div bind:this={container} class="chart-container"></div>
-
-<style>
-  /* Add styling for the chart container if needed */
-  .chart-container {
-    text-align: center;
-  }
-</style>
+<div on:mousemove={onMousemove}>
+  <LinePlot data={data} />
+</div>
 
