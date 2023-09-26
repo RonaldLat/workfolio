6bdb94435842b115d3607b4943d595a04ba50241
Ronald Otieno <ronnlats@gmail.com>
2023-09-26 10:32:37 +0300

    chart title

--- src/routes/d3/+page.svelte
+++ src/routes/d3/+page.svelte
@@ -1,11 +1,10 @@
 <script>
 	import Scatter from '$lib/components/d3/Scatter.svelte';
 
-
-
 </script>
 
 <div class="min-h-screen w-full">
 	<h1 class="text-2xl bold">D3 Playground</h1>
+	<h2 class="text-2xl bold">EPL Tables</h2>
 	<Scatter />
 </div>
