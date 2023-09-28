eb7a5a46dad1245d7392d834cbdcd418abb090c5
Ronald Otieno <ronnlats@gmail.com>
2023-09-28 17:12:06 +0300

    Monokai theme

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const mdsvexOptions = {
   extensions: ['.md'],
   highlight: {
     highlighter: async (code, lang = 'text') => {
-      const highlighter = await shiki.getHighlighter({ theme: 'poimandres' })
+      const highlighter = await shiki.getHighlighter({ theme: 'monokai' })
       const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
       return `{@html \`${html}\` }`
     }
