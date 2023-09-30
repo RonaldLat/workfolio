acd2dada2adeb24941b61471da465eb84ad9ecc3
Ronald Otieno <ronnlats@gmail.com>
2023-09-30 22:15:09 +0300

    line chart

---
+++ src/lib/components/d3/LineChart.svelte
@@ -0,0 +1,119 @@
+<script>
+  import * as d3 from "d3";
+
+  // Receive plot data as prop.
+  import data from  '$lib/data/stock'
+	import { onMount } from "svelte";
+
+  // The chart dimensions and margins as optional props.
+  export let width = 928;
+  export let height = 500;
+  export let marginTop = 20;
+  export let marginRight = 30;
+  export let marginBottom = 30;
+  export let marginLeft = 40;
+
+
+let epl =[]
+onMount(async function(){
+        epl = await d3.json(`https://www.thesportsdb.com/api/v1/json/3/lookuptable.php?l=4328&s=2022-2023`);
+        epl = epl.table.slice(0,6)
+    })
+        $:console.log('line',epl)
+
+  // Create the x (horizontal position) scale.
+  const xScale = d3.scaleUtc(
+    d3.extent(data, (d) => new Date(d.date)),
+    [marginLeft, width - marginRight]
+  );
+
+  // Create the y (vertical position) scale.
+  const yScale = d3.scaleLinear(
+    [0, d3.max(data, (d) => d.close)],
+    [height - marginBottom, marginTop]
+  );
+
+  // Create the line generator.
+  const line = d3
+    .line()
+    .x((d) => xScale(new Date(d.date)))
+    .y((d) => yScale(d.close));
+</script>
+
+<svg
+  {width}
+  {height}
+  viewBox="0 0 {width} {height}"
+  class="w-full h-auto"
+>
+  <!-- X-Axis -->
+  <g transform="translate(0,{height - marginBottom})">
+    <line stroke="currentColor" x1={marginLeft - 6} x2={width} />
+
+    {#each xScale.ticks() as tick}
+      <!-- X-Axis Ticks -->
+      <line
+        stroke="currentColor"
+        x1={xScale(tick)}
+        x2={xScale(tick)}
+        y1={0}
+        y2={6}
+      />
+
+      <!-- X-Axis Tick Labels -->
+      <text fill="currentColor" text-anchor="middle" x={xScale(tick)} y={22}>
+        {tick.getFullYear()}
+      </text>
+    {/each}
+  </g>
+
+  <!-- Y-Axis and Grid Lines -->
+  <g transform="translate({marginLeft},0)">
+    {#each yScale.ticks() as tick}
+      {#if tick !== 0}
+        <!--
+          Grid Lines.
+          Note: First line is skipped since the x-axis is already present at 0.
+        -->
+        <line
+          stroke="currentColor"
+          stroke-opacity="0.1"
+          x1={0}
+          x2={width - marginLeft}
+          y1={yScale(tick)}
+          y2={yScale(tick)}
+        />
+
+        <!--
+          Y-Axis Ticks.
+          Note: First tick is skipped since the x-axis already acts as a tick.
+        -->
+        <line
+          stroke="currentColor"
+          x1={0}
+          x2={-6}
+          y1={yScale(tick)}
+          y2={yScale(tick)}
+        />
+      {/if}
+
+      <!-- Y-Axis Tick Labels -->
+      <text
+        fill="currentColor"
+        text-anchor="end"
+        dominant-baseline="middle"
+        x={-9}
+        y={yScale(tick)}
+      >
+        {tick}
+      </text>
+    {/each}
+
+    <!-- Y-Axis Label -->
+    <text fill="currentColor" text-anchor="start" x={-marginLeft} y={15}>
+      ↑ Daily close ($)
+    </text>
+  </g>
+
+  <path fill="none" class="stroke-violet-800" stroke-width="1.5" d={line(data)} />
+</svg>
--- src/routes/d3/+page.svelte
+++ src/routes/d3/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import FirstChart from '$lib/components/d3/FirstChart.svelte';
+	import LineChart from '$lib/components/d3/LineChart.svelte';
 	import Scatter from '$lib/components/d3/Scatter.svelte';
 	import SecondChart from '$lib/components/d3/SecondChart.svelte';
 </script>
@@ -8,4 +9,5 @@
 	<h1 class="text-2xl bold">D3 Playground</h1>
 	<h2 class="text-2xl bold">EPL Tables</h2>
 	<SecondChart/>
+    <LineChart/>
 </div>
