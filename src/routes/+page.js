d07ac1d94b9e3422ddf56d2c63a7f5b160efa547
Ronald Otieno <ronnlats@gmail.com>
2023-09-30 06:15:46 +0000

    Daily Stoic

---
+++ src/routes/+page.js
@@ -0,0 +1,9 @@
+export const load = async ({ fetch }) => {
+	const response = await fetch(`https://stoic-quotes.com/api/quote`);
+	const quote = await response.json();
+
+	return {
+		quote
+	};
+};
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,16 @@
 	import Home from '$lib/components/ui/Home.svelte';
 	import Portofolio from '$lib/components/Portofolio.svelte';
 	import Tools from '$lib/components/Tools.svelte';
+	import DailyStoic from '$lib/components/DailyStoic.svelte';
+  export let data
+
+  const {quote} = data
+
 </script>
 
 <div class="min-h-screen font-[Ubuntu] bg-myColor-50">
 	<Home />
+    <DailyStoic {quote}/>
 	<Portofolio />
 	<Tools />
 </div>
