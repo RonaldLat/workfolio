2cadad96f42a5af239a3b5e128a8277095f8ddcd
Ronald Otieno <ronnlats@gmail.com>
2023-10-01 21:50:27 +0300

    success with installing gsap and scrolltrigger

--- codePractice/pythonTricks.py
+++ codePractice/pythonTricks.py
@@ -1,8 +1,16 @@
-def arrayConcatenation( nums):
-    ans =[]
-    for i in range(3):
-        for n in nums:
-            ans.append(n)
-    return ans
-ans = arrayConcatenation([1,2,4])
-print(ans)
+def merge_sorted(list1, list2):
+    sorted =[]
+    for l1, l2 in zip(list1, list2):
+        if l1 == l2 :
+            sorted.append(l1)
+            sorted.append(l2)
+        elif l1 <l2:
+            sorted.append(l1)
+            sorted.append(l2)
+
+    print(sorted)
+
+list1 = [1,2,4]
+list2 = [1,3,4]
+merge_sorted(list1, list2)
+
