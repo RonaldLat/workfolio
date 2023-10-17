244b2ab5c288f763f82c02105bbf1dc79bc96e84
Ronald Otieno <ronnlats@gmail.com>
2023-10-17 09:09:54 +0300

    Update +page.js

--- src/routes/blog/+page.js
+++ src/routes/blog/+page.js
@@ -1,7 +1,7 @@
 export const load = async ({ fetch }) => {
   const response = await fetch(`/api/posts`);
   const posts = await response.json();
-  console.log(posts)
+  console.log('posts: ',posts)
 
   return {
     posts
