61576a08c7483d9050342a687ea05eb5ba5c86dc
Ronald Otieno <ronnlats@gmail.com>
2024-03-04 12:04:46 +0000

    color adjustment for portofolio section

--- src/lib/components/Portofolio.svelte
+++ src/lib/components/Portofolio.svelte
@@ -55,18 +55,18 @@ scroll(
 
 <div class="body mt-14 ">
 <div class="container">
-  <section data-bgcolor="#bcb8ad" data-textcolor="#032f35" class="bg-myColor-600 h-[50vh] text-gray-900 hidden">
+  <section data-bgcolor="#bcb8ad" data-textcolor="#032f35" class="bg-myColor-800 h-[50vh] text-gray-900 hidden">
     <div>
-      <h1 data-scroll data-scroll-speed="1"><span>My</span> <span>Projects</span> </h1>
+      <h1 data-scroll data-scroll-speed="1"><span>My</span> <span>Projectz</span> </h1>
     </div>
 
   </section>
 
-  <section bind:this={sectionPin} id="sectionPin" class="bg-transparent min-h-screen">
-    <div bind:this={slidingContent} class="pin-wrap flex gap-x-2 text-gray-800">
-      <h2>My Projects</h2>
+  <section bind:this={sectionPin} id="sectionPin" class=" min-h-screen bg-myColor-200 rounded-md">
+    <div bind:this={slidingContent} class="pin-wrap flex gap-x-2 text-gray-800  ">
+      <h2 class="text-7xl">My Projects</h2>
       {#each projects as project }
-      <div class="flex flex-col text-gray-500   border-2 py-8 h-auto ">
+      <div class="flex flex-col text-gray-700   border-2 border-gray-700 rounded-md py-8 h-auto ">
       <img class="h-full w-auto py-6" src={project.img} alt={project.alt}>
       <span>{project.name}</span>
       </div>
