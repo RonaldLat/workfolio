7ed424504c269c285b82fc2ac28e93187b2d32ff
Ronald Otieno <ronnlats@gmail.com>
2024-03-30 00:31:27 +0300

    remove DailyStoic
    modified main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
   import Home from '$lib/components/ui/Home.svelte';
   import Portofolio from '$lib/components/Portofolio.svelte';
   import Tools from '$lib/components/Tools.svelte';
-  import DailyStoic from '$lib/components/DailyStoic.svelte';
   import FeaturedPosts from '$lib/components/ui/FeaturedPosts.svelte';
   export let data;
 
@@ -12,9 +11,6 @@
 
 <div class="min-h-screen w-full relative font-[Ubuntu] bg-myColor-50 px-2 lg:px-4">
   <Home />
-  {#if quote}
-    <DailyStoic {quote} />
-  {/if}
   <Portofolio />
   <FeaturedPosts />
 </div>
