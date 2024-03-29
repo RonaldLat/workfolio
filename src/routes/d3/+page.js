a6ea9b6401c35163ee5e1454cd1ba758654bae4d
Ronald Otieno <ronald@bunsenlabs.ronald>
2024-03-29 17:03:14 +0300

    cleanup

--- src/routes/d3/+page.js
+++
@@ -1,8 +0,0 @@
-export const load = async ({ fetch }) => {
-  //const response = await fetch(`https://kamalabot.github.io/json_data_repo/glass_door_DSci_datasets/gd_salary_data_cleaned.json`);
-  //const salaries = await response.json();
-
-  return {
-    //salaries
-  };
-};
