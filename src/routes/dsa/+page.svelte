2bfcf9ae678be3d5710c3408dab501f6e4e9f204
Ronald Otieno <ronnlats@gmail.com>
2024-03-30 00:46:11 +0300

    more cleanup

--- src/routes/dsa/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import BreathingGrids from '$lib/components/BreathingGrids.svelte';
-  import QuickSort from '$lib/components/dsa/QuickSort.svelte';
-</script>
-
-<h2>Data Structures and Algorithms</h2>
-<QuickSort />
-<BreathingGrids />
