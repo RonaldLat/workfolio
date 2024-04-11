3ad25e2cf327ebe844e670801b57bdc42ecf0998
Ronald Otieno <ronnlats@gmail.com>
2024-04-11 13:23:41 +0300

    one grid on mobile

--- src/lib/components/Featured.svelte
+++ src/lib/components/Featured.svelte
@@ -14,7 +14,7 @@
 </div>
 
 <div
-	class="overflow-hidden w-full mx-auto grid grid-cols-2 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 md:gap-x-14 mt-10 mb-5 "
+	class="overflow-hidden w-full mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 md:gap-x-14 mt-10 mb-5 "
 >
 	{#each products as product}
 		<ProductCard3 image="/files/{product.imageUrl}" name={product.name} id={product.id} category={product.category} price={product.price} old_price={product.old_price}/>
