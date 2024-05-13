839e3eb77708e0157472992c235847f056158c11
Ronald Otieno <ronnlats@gmail.com>
2024-05-13 21:22:56 +0000

    minor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,36 @@
 <script>
 	import Home from '$lib/Home.svelte';
 
-  export let data;
-  const {leads, } = data;
-  console.log('Root page',data.session)
-
+	export let data;
+	const { leads } = data;
+	console.log('Root page', data.session);
 </script>
 
 <main class="bg-gradient-to-r mt-20 relative from-gray-100 to-gray-300">
+	{#if data.session}
+		<p>Welcome, {data.session.user.email}</p>
+	{:else}
+		<p class="text-rose-600 text-center">not loggen in home page</p>
+	{/if}
 
-{#if data.session}
-<p>Welcome, {data.session.user.email}</p>
-{:else}
-<p class="text-rose-600 text-center">not loggen in home page</p>
-
-{/if}
-
-<ul class="hidden">
-{#if leads}
-<p>{leads.data.length} prospects</p>
-{#each leads.data as lead }
-<li class="bg-slate-400 p-1 rounded-sm w-full my-2 text-sm  max-w-md">
-<p><span class=" font-semibold">Name: </span><span>{lead.name}</span></p>
-<p> <span class="font-semibold">Phone: </span><a href="tel:{lead.phone_number}" class=" w-fit underline text-sky-600">{lead.phone_number}</a></p>
-<p class="">Website: {lead.website}</p>
-<p class="">Verified: {lead.verified}</p>
-<span>Called: </span><input type="checkbox">
-</li>
-
-{/each}
-{/if}
-</ul>
+	<ul class="hidden">
+		{#if leads}
+			<p>{leads.data.length} prospects</p>
+			{#each leads.data as lead}
+				<li class="bg-slate-400 p-1 rounded-sm w-full my-2 text-sm max-w-md">
+					<p><span class=" font-semibold">Name: </span><span>{lead.name}</span></p>
+					<p>
+						<span class="font-semibold">Phone: </span><a
+							href="tel:{lead.phone_number}"
+							class=" w-fit underline text-sky-600">{lead.phone_number}</a
+						>
+					</p>
+					<p class="">Website: {lead.website}</p>
+					<p class="">Verified: {lead.verified}</p>
+					<span>Called: </span><input type="checkbox" />
+				</li>
+			{/each}
+		{/if}
+	</ul>
 	<Home />
 </main>
--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,4 +1,4 @@
-<p>Dashboard layout</p>
-<main  class="bg-sky-400 h-72">
-<slot/>
+<h1 class="text-2xl font-semibold">Dashboard</h1>
+<main class="bg-sky-400 h-72">
+	<slot />
 </main>
